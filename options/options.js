0ced939769164d5806ebd7152bd22a33d5a1e699
scartag <tag@crowdbotics.com>
2022-10-26 13:37:02 +0100

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "revastudio_dev_39676",
   url: "https://revastudio_dev_39676.botics.co",
   api: "https://revastudio_dev_39676.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
---
+++ screens/Untitled1/index.js
@@ -0,0 +1,34 @@
+import { Pressable } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled1 = ({
+  navigation
+}) => {
+  return <View style={_styles.NQjALIdV}>
+      <Pressable onPress={() => navigation.navigate("Untitled2")} style={_styles.wezIWKHU}><View style={_styles.fbyFUaMt}></View></Pressable></View>;
+};
+
+export default Untitled1;
+
+const _styles = StyleSheet.create({
+  NQjALIdV: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  },
+  fbyFUaMt: {
+    left: 70,
+    top: 149,
+    position: "absolute",
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  wezIWKHU: {
+    position: "unset"
+  }
+});
