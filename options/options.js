19470f8a166d1ae81273c1fdc22c2f70d104ff5b
scartag <tag@crowdbotics.com>
2022-11-09 16:23:41 +0000

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,9 @@
+const styles = StyleSheet.create({});
 export const globalOptions = {
-  name: "revastudio_dev_39676",
-  url: "https://revastudio_dev_39676.botics.co",
-  api: "https://revastudio_dev_39676.botics.co/api/v1"
+  "name": "revastudio_dev_39676",
+  "url": "https://revastudio_dev_39676.botics.co",
+  "api": "https://revastudio_dev_39676.botics.co/api/v1"
 };
 export const modulesOptions = {
-  "@modules/app-menu": {
-    "copy": "Routes available!"
-  }
+  "@modules/app-menu": {}
 };
